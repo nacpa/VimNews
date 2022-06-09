6c9ae23a0601d45dee7e74eca7ba516ea0748ea6
Nachiketa Pandey <nacpa360@gmail.com>
2022-06-09 11:23:47 +0530

    update 1.0.3

--- lib/View/BookMark.dart
+++ lib/View/BookMark.dart
@@ -3,103 +3,111 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:vimnws/Helper/Dimension.dart';
 
-class BookMark extends StatefulWidget {
-  const BookMark({Key? key}) : super(key: key);
-
-  @override
-  State<BookMark> createState() => _BookMarkState();
-}
-
-class _BookMarkState extends State<BookMark> {
-  bool _loading=true;
-  final _fireData=FirebaseFirestore.instance;
-  List<String> UrlToImiage=[];
-  List<String> Title=[];
-  late int Length=0;
 
 
-  Future GetBookMark()async {
-    final Data= await  _fireData.collection("data").get();
-    Length=Data.size;
-    Data.docs.forEach((element) {
-      UrlToImiage.add(element['urlToImage'].toString());
-      Title.add(element['title']);
-      _loading=false;
-      // print(element['title'].toString());
-      // print(element['urlToImage'].toString());
-      print(Length);
-    });
-  }
+final _FireData = FirebaseFirestore.instance.collection("data");
+class bookMark extends StatelessWidget {
+  const bookMark({Key? key}) : super(key: key);
 
-
-
-
-  @override
-  void initState() {
-    // TODO: implement initState
-    super.initState();
-    setState(() {
-      GetBookMark();
-      _loading=false;
-    });
-
-    print("NAcpa");
-
-
-  }
   @override
   Widget build(BuildContext context) {
-    GetBookMark();
-
-
-
-    return Scaffold(appBar: AppBar(
-      backgroundColor: Colors.white,
-      elevation: 0,
-      title: Row(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          Text(
-            "Vim",
-            style: TextStyle(
-                color: Colors.black, fontSize: Dimension.width10! * 3),
+    return StreamBuilder<QuerySnapshot>(stream: _FireData.orderBy("urlToImage",descending: true).snapshots(),builder: (context,snapshot){
+      if (snapshot.data == null) {
+        return Center(
+          child: Scaffold(
+            body: Container(
+                height: Dim.Hight10*5.0,
+                width: Dim.Hight10*5.0,
+                color: Colors.white,
+                child: Center(child: CircularProgressIndicator(color: Colors.orange,))),
           ),
-          Text(
-            "News  ",
-            style: TextStyle(
-              color: Colors.blue,
-              fontSize: Dimension.width10! * 3,
-            ),
-          )
-        ],
-      ),
+        );
+      };
+      final BookMarkList=snapshot.data?.docs;
+      List<Map<String,String>> BookMarks=[];
+      if(true){
+        BookMarkList?.forEach((element) { BookMarks.add({"Tittle":element["title"],"Url":element["urlToImage"]});});
 
-    ),
-    body: _loading?Container(child: CircularProgressIndicator(color: Colors.purple,),) :Container(height: double.maxFinite,width: double.maxFinite,
-      margin: EdgeInsets.all(4),color: Colors.white,
-      child: ListView.builder(itemCount:Length,shrinkWrap: true,
-          itemBuilder: (context,i){ GetBookMark();
-          return  Container(margin: EdgeInsets.only(bottom: Dimension.width10!*2,top: Dimension.width10!*2),
-            height: Dimension.Screenheight!/9,width: Dimension.ScreenWidth!/0.9,
-            decoration: BoxDecoration(color: Colors.white,borderRadius: BorderRadius.circular(20),boxShadow: [BoxShadow(offset: Offset(0,4),blurRadius: 3,spreadRadius: 3,color: Colors.grey.shade400)]),
-            child: Row(mainAxisAlignment: MainAxisAlignment.start,
-              children: [
-                Container(height:double.maxFinite,width: Dimension.width10!*10,
-                    decoration: BoxDecoration(borderRadius: BorderRadius.circular(20),
-                      image: DecorationImage(fit: BoxFit.fill,
-                          image: NetworkImage(UrlToImiage[i])),
-                    )
-
+      }
+      return Scaffold(
+        appBar: AppBar(
+          backgroundColor: Colors.white,
+          elevation: 0,
+          title: Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text(
+                "Vim",
+                style: TextStyle(
+                    color: Colors.black, fontSize: Dimension.width10! * 3),
+              ),
+              Text(
+                "News  ",
+                style: TextStyle(
+                  color: Colors.blue,
+                  fontSize: Dimension.width10! * 3,
                 ),
-                Container(height:double.maxFinite,width: Dimension.ScreenWidth!/1.6,margin: EdgeInsets.all(10),
-                    child: Text(Title[i],overflow: TextOverflow.ellipsis,maxLines: 4,style: TextStyle(fontWeight: FontWeight.bold,fontSize: Dimension.width10!*2),)
-                    ,decoration: BoxDecoration(
-                    )
-
-                ),
-              ],),
-          );
-          }),
-    ),);
+              )
+            ],
+          ),
+        ),
+        body: Container(
+          height: double.maxFinite,
+          width: double.maxFinite,
+          margin: EdgeInsets.all(4),
+          color: Colors.white,
+          child: ListView.builder(
+              itemCount: BookMarks.length,
+              shrinkWrap: true,
+              itemBuilder: (context, i) {
+                return Container(
+                  margin: EdgeInsets.only(
+                      bottom: Dimension.width10! * 2,
+                      top: Dimension.width10! * 2),
+                  height: Dimension.Screenheight! / 9,
+                  width: Dimension.ScreenWidth! / 0.9,
+                  decoration: BoxDecoration(
+                      color: Colors.white,
+                      borderRadius: BorderRadius.circular(20),
+                      boxShadow: [
+                        BoxShadow(
+                            offset: Offset(0, 4),
+                            blurRadius: 3,
+                            spreadRadius: 3,
+                            color: Colors.grey.shade400)
+                      ]),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.start,
+                    children: [
+                      Container(
+                          height: double.maxFinite,
+                          width: Dimension.width10! * 10,
+                          decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(20),
+                            image: DecorationImage(
+                                fit: BoxFit.fill,
+                                image: NetworkImage(BookMarks[i]["Url"].toString())),
+                          )),
+                      Container(
+                          height: double.maxFinite,
+                          width: Dimension.ScreenWidth! / 1.6,
+                          margin: EdgeInsets.all(10),
+                          child: Text(
+                            BookMarks[i]["Tittle"].toString(),
+                            overflow: TextOverflow.ellipsis,
+                            maxLines: 4,
+                            style: TextStyle(
+                                fontWeight: FontWeight.bold,
+                                fontSize: Dimension.width10! * 2),
+                          ),
+                          decoration: BoxDecoration()),
+                    ],
+                  ),
+                );
+              }),
+        ),
+      );
+    },);
   }
 }
+
